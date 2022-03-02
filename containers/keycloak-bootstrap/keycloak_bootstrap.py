dcff1eaf5e3106b9426726b36b9177e44ddf56dc
Mikhail Ustiantsev <39340048+mustyantsev@users.noreply.github.com>
2022-03-02 22:28:39 +0200

    PLAT-1519 Python Add PKI authN support (#72)
    * PLAT-1519 Python Add PKI authN support
    
    * PLAT-1519 upgrade keycloak python lib to 0.27
    
    * PLAT-1519 get debug logs
    
    * PLAT-1519 rearrange auth flow creation
    
    * PLAT-1519 debug execution
    
    * PLAT-1519 debug 2
    
    * PLAT-1519 check if provide is not none
    
    * PLAT-1519 remove debug logs

--- containers/keycloak-bootstrap/keycloak_bootstrap.py
+++ containers/keycloak-bootstrap/keycloak_bootstrap.py
@@ -447,6 +447,10 @@ def createAuthFlowX509(keycloak_admin, realm_name, flow_name, provider_name):
             )
 
     flows_execution = keycloak_admin.get_authentication_flow_executions(flow_name)
+    data_direct_grant_flow = keycloak_admin.get_authentication_flow_executions("X509_Direct_Grant")
+    for key in data_direct_grant_flow:
+        if key.get('providerId') is not None and key.get('providerId') != 'direct-grant-auth-x509-username':
+            keycloak_admin.delete_authentication_flow_execution(key.get('id'))
     filtered_flow = check_matched({"providerId": provider_name}, flows_execution)
     if filtered_flow:
         payload_config = {
@@ -455,11 +459,11 @@ def createAuthFlowX509(keycloak_admin, realm_name, flow_name, provider_name):
                 "x509-cert-auth.canonical-dn-enabled": "false",
                 "x509-cert-auth.mapper-selection.user-attribute-name": "usercertificate",
                 "x509-cert-auth.serialnumber-hex-enabled": "false",
-                "x509-cert-auth.regular-expression": "(.*?)(?:$)",
+                "x509-cert-auth.regular-expression": "CN=(.*?)(?:$),",
                 "x509-cert-auth.mapper-selection": "Username or Email",
                 "x509-cert-auth.crl-relative-path": "crl.pem",
                 "x509-cert-auth.crldp-checking-enabled": "false",
-                "x509-cert-auth.mapping-source-selection": "Subject's e-mail",
+                "x509-cert-auth.mapping-source-selection": "Subject's Common Name",
                 "x509-cert-auth.timestamp-validation-enabled": "true",
             },
         }
@@ -604,12 +608,6 @@ def createTDFPKIRealm(kc_admin_user, kc_admin_pass, kc_url):
     # Create test client configured for browser auth flow
     createTestClientForBrowserAuthFlow(keycloak_admin)
 
-    # X.509 Client Certificate Authentication to a Browser Flow
-    # https://www.keycloak.org/docs/latest/server_admin/index.html#adding-x-509-client-certificate-authentication-to-a-browser-flow
-    createAuthFlowX509(
-        keycloak_admin, realm_name, "X509_Browser", "auth-x509-client-username-form"
-    )
-
     # X.509 Client Certificate Authentication to a Direct Grant Flow
     # https://www.keycloak.org/docs/latest/server_admin/index.html#adding-x-509-client-certificate-authentication-to-a-direct-grant-flow
     createAuthFlowX509(
@@ -617,6 +615,12 @@ def createTDFPKIRealm(kc_admin_user, kc_admin_pass, kc_url):
         realm_name,
         "X509_Direct_Grant",
         "direct-grant-auth-x509-username",
+    )
+
+    # X.509 Client Certificate Authentication to a Browser Flow
+    # https://www.keycloak.org/docs/latest/server_admin/index.html#adding-x-509-client-certificate-authentication-to-a-browser-flow
+    createAuthFlowX509(
+        keycloak_admin, realm_name, "X509_Browser", "auth-x509-client-username-form"
     )
 
     createTestClientForX509Flow(keycloak_admin)
