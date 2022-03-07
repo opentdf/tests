d5254b7f7d0ec323ef49ddd8d57a7d392a4451d6
Ben Leggett <854255+bleggett@users.noreply.github.com>
2022-03-07 12:09:39 -0500

    Remove config that crashes Keycloak (#78)
    * Remove config that crashes Keycloak
    
    * Simplify logging
    
    * Update entitlements_bootstrap.py
    
    Co-authored-by: David Mihalcik <dmihalcik@virtru.com>

--- containers/keycloak-bootstrap/keycloak_bootstrap.py
+++ containers/keycloak-bootstrap/keycloak_bootstrap.py
@@ -447,10 +447,6 @@ def createAuthFlowX509(keycloak_admin, realm_name, flow_name, provider_name):
             )
 
     flows_execution = keycloak_admin.get_authentication_flow_executions(flow_name)
-    data_direct_grant_flow = keycloak_admin.get_authentication_flow_executions("X509_Direct_Grant")
-    for key in data_direct_grant_flow:
-        if key.get('providerId') is not None and key.get('providerId') != 'direct-grant-auth-x509-username':
-            keycloak_admin.delete_authentication_flow_execution(key.get('id'))
     filtered_flow = check_matched({"providerId": provider_name}, flows_execution)
     if filtered_flow:
         payload_config = {
