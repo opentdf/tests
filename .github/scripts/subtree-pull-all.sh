7b8a4c3a3f4cb74e595c7170f14b318ef06f6105
Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>
2022-03-11 16:13:44 -0500

    ⚙️ Helper automation for subtree pull (#15)
    - Helper script to pull latest changes from subtrees
    - Adds shellcheck to it

---
+++ .github/scripts/subtree-pull-all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+projects=( backend frontend tdf3-js )
+now=$(date +"%Y-%m-%dT%H:%M:%S%z")
+
+for project in "${projects[@]}"; do
+  m="🔀 git subtree pull $project at $now"
+  if ! git subtree pull -m "$m" -P "$project" "git@github.com:/opentdf/$project" main; then
+    echo "Unable to pull $project"
+    exit 1
+  fi
+done
+
+if ! git log -1 | grep "at $now"; then
+  echo "No changes found"
+fi
+
