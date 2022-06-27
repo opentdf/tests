b9930076b3198e8bc6a5bd031cb8cf5fe2b0e7c3
sivanov <sivanov.ctr@virtru.com>
2022-06-27 19:28:53 +0300

    path __dirname added

--- client-web-test/tests/basic.spec.ts
+++ client-web-test/tests/basic.spec.ts
@@ -1,7 +1,12 @@
 import { expect } from '@playwright/test';
 import { authorize } from './helpers/operations';
 import { test } from './helpers/fixtures';
+// @ts-ignore
+import toString from 'stream-to-string';
 import fs from 'fs';
+import path from 'path';
+
+const originalText = fs.readFileSync(path.join(__dirname, 'file.txt'), 'utf8');
 
 test.describe('<TDF3JS/>', () => {
     test.beforeEach(async ({ page }) => {
@@ -12,17 +17,15 @@ test.describe('<TDF3JS/>', () => {
     test('should use FileClient to encrypt/decrypt file text', async ({ page }) => {
         const header = page.locator('h2:has-text("Attributes")');
         await expect(header).toBeVisible();
-        const originalText = fs.readFileSync('./file.txt', 'utf8');
-        throw originalText;
-        // // @ts-ignore
-        // const [ download ] = await Promise.all([
-        //     page.waitForEvent('download'), // wait for download to start
-        //     page.locator('id=username').setInputFiles("./file.txt")
-        // ]);
-        // // wait for download to complete
-        // const path = await download.path();
-        // const decryptedText = fs.readFileSync(path, 'utf8');
-        //
-        // expect(decryptedText).toEqual(originalText);
+        // @ts-ignore
+        const [ download ] = await Promise.all([
+            page.waitForEvent('download'), // wait for download to start
+            page.locator('id=username').setInputFiles("./file.txt")
+        ]);
+        // wait for download to complete
+        const stream = await download.createReadStream();
+        const decryptedText = await toString(stream);
+
+        expect(decryptedText).toEqual(originalText);
     });
 });
--- client-web-test/tests/node.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-(() => {
-	console.log(fs.readFileSync('./file.txt', 'utf8'));
-	console.log(__dirname)
-})();
