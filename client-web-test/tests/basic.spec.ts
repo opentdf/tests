d49186aa20a5cd2f646e32ed217afc96916e523f
sivanov <sivanov.ctr@virtru.com>
2022-06-29 16:26:55 +0300

    Try to remove flakiness

--- client-web-test/tests/basic.spec.ts
+++ client-web-test/tests/basic.spec.ts
@@ -7,24 +7,25 @@ import fs from 'fs';
 import path from 'path';
 
 const originalText = fs.readFileSync(path.join(__dirname, 'file.txt'), 'utf8');
+let page;
 
 test.describe('<TDF3JS/>', () => {
-
-    test('should use FileClient to encrypt/decrypt file text', async ({ browser }) => {
+    test.beforeEach(async ({ browser }) => {
         const context = await browser.newContext({ acceptDownloads: true });
-        const page = await context.newPage();
+        page = await context.newPage();
         await authorize(page);
         await page.goto('/');
+    });
 
-        const header = page.locator('h2:has-text("Attributes")');
+    test('should use FileClient to encrypt/decrypt file text', async () => {
+        const header = await page.locator('h2:has-text("Attributes")');
         await expect(header).toBeVisible();
-        await page.locator("input[type=\"file\"]").setInputFiles(path.join(__dirname, 'file.txt'));
+        page.locator("input[type=\"file\"]").setInputFiles(path.join(__dirname, 'file.txt'));
         // @ts-ignore
         const download = await page.waitForEvent('download'); // wait for download to start
         // wait for download to complete
         const stream = await download.createReadStream();
         const decryptedText = await toString(stream);
-
         expect(decryptedText).toEqual(originalText);
     });
 });
