1715225f8dfa7d65fbf6941b470664cb64272e17
sivanov <sivanov.ctr@virtru.com>
2022-06-29 17:10:14 +0300

    will parallel fix flakiness?

--- client-web-test/tests/basic.spec.ts
+++ client-web-test/tests/basic.spec.ts
@@ -1,31 +1,28 @@
-import { expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { authorize } from './helpers/operations';
-import { test } from './helpers/fixtures';
-// @ts-ignore
 import toString from 'stream-to-string';
 import fs from 'fs';
 import path from 'path';
 
 const originalText = fs.readFileSync(path.join(__dirname, 'file.txt'), 'utf8');
-let page;
 
-test.describe('<TDF3JS/>', () => {
-    test.beforeEach(async ({ browser }) => {
-        const context = await browser.newContext({ acceptDownloads: true });
-        page = await context.newPage();
-        await authorize(page);
-        await page.goto('/');
-    });
+// @ts-ignore
+test.describe.configure({ mode: 'parallel' });
 
-    test('should use FileClient to encrypt/decrypt file text', async () => {
-        const header = await page.locator('h2:has-text("Attributes")');
-        await expect(header).toBeVisible();
-        page.locator("input[type=\"file\"]").setInputFiles(path.join(__dirname, 'file.txt'));
-        // @ts-ignore
-        const download = await page.waitForEvent('download'); // wait for download to start
-        // wait for download to complete
-        const stream = await download.createReadStream();
-        const decryptedText = await toString(stream);
-        expect(decryptedText).toEqual(originalText);
-    });
+test('should use FileClient to encrypt/decrypt file text', async ({ browser }) => {
+    const context = await browser.newContext({ acceptDownloads: true });
+    const page = await context.newPage();
+    await authorize(page);
+    await page.goto('/');
+
+    const header = await page.locator('h2:has-text("Attributes")');
+    await expect(header).toBeVisible();
+    await page.locator("input[type=\"file\"]").setInputFiles(path.join(__dirname, 'file.txt'));
+    // @ts-ignore
+    const download = await page.waitForEvent('download'); // wait for download to start
+    // wait for download to complete
+    const stream = await download.createReadStream();
+    const decryptedText = await toString(stream);
+
+    expect(decryptedText).toEqual(originalText);
 });
