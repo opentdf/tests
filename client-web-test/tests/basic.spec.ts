80c9282d84967e1cfe821c3990b8ba3c79689586
sivanov <sivanov.ctr@virtru.com>
2022-06-29 15:00:12 +0300

    auth fixed

--- client-web-test/tests/basic.spec.ts
+++ client-web-test/tests/basic.spec.ts
@@ -9,21 +9,18 @@ import path from 'path';
 const originalText = fs.readFileSync(path.join(__dirname, 'file.txt'), 'utf8');
 
 test.describe('<TDF3JS/>', () => {
-    test.beforeEach(async ({ page }) => {
-        await authorize(page);
-    });
 
     test('should use FileClient to encrypt/decrypt file text', async ({ browser }) => {
         const context = await browser.newContext({ acceptDownloads: true });
         const page = await context.newPage();
+        await authorize(page);
         await page.goto('/');
 
         const header = page.locator('h2:has-text("Attributes")');
         await expect(header).toBeVisible();
-        await page.locator("input[type=\"1file\"]").setInputFiles(path.join(__dirname, 'file.txt'));
-        // await page.locator("input[type=\"file\"]").setInputFiles(path.join(__dirname, 'file.txt'));
-
-        const download = await page.waitForEvent('download')
+        await page.locator("input[type=\"111file\"]").setInputFiles(path.join(__dirname, 'file.txt'));
+        // @ts-ignore
+        const download = await page.waitForEvent('download'); // wait for download to start
         // wait for download to complete
         const stream = await download.createReadStream();
         const decryptedText = await toString(stream);
