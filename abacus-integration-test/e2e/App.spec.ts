a06a38dc53a441e885fea6033a08c80899cf77f5
sievdokymov-virtru <sergii.ievdokymov@virtru.com>
2022-10-31 13:28:10 +0200

    Playwright tests on CI 2-24

---
+++ abacus-integration-test/e2e/App.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import { selectors } from "./helpers/selectors";
+import { authorize } from "./helpers/operations";
+
+test.describe('<App/>', () => {
+  test.beforeEach(async ({ page }) => {
+    await authorize(page);
+    await page.goto('/');
+  });
+
+  test('renders initially', async ({ page }) => {
+    const header = page.locator('h2', { hasText: "Attributes" });
+    await expect(header).toBeVisible();
+  });
+
+  test('should get authorization token', async ({ page }) => {
+    const logoutButton = page.locator(selectors.logoutButton);
+    expect(logoutButton).toBeTruthy();
+  });
+
+  test('should be able to log out', async ({ page }) => {
+    await page.goto('/attributes');
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(selectors.logoutButton),
+    ])
+    await page.waitForSelector(selectors.loginButton);
+    // check that data isn't shown
+    const authorityDropdown = page.locator(".ant-select-selector >> nth=1")
+    await authorityDropdown.click()
+    await expect(page.locator('.ant-empty-description')).toHaveText('No Data')
+  });
+});
---
+++ abacus-integration-test/e2e/helpers/selectors.ts
@@ -0,0 +1,79 @@
+export const selectors = {
+    sectionTab: '.nav-link',
+    loginButton: '[data-test-id=login-button]',
+    logoutButton: '[data-test-id=logout-button]',
+    loginScreen: {
+        usernameField: '#username',
+        passwordField: '#password',
+        submitButton: '#kc-login'
+    },
+    secondaryHeader: 'h2',
+    realmSelector: '#rc_select_0',
+    attributesPage: {
+        attributesHeader: {
+            authorityDropdownButton: "#select-authorities-button",
+            itemsQuantityIndicator: '.ant-pagination-total-text',
+            sortByToolbarButton: "#sort-by-button",
+            filtersToolbarButton: "#filters-button",
+            filterModal: {
+                ruleInputField: '#filter_rule',
+                nameInputField: '#filter_name',
+                orderInputField: '#filter_order',
+                submitBtn: '#submit-filter-button',
+                clearBtn: '#clear-filter-button'
+            },
+        },
+        attributeDetailsSection: {
+            editRuleButton: '#edit',
+            deleteAttributeButton: '#delete-attribute',
+            confirmAttributeDeletionModal: {
+                cancelDeletionBtn: '#cancel-attribute-deletion',
+                confirmDeletionBtn: '#confirm-attribute-deletion',
+            },
+            closeDetailsSectionButton: "#close-details-button",
+            ruleDropdown: '.attribute-rule__select',
+            saveRuleButton: '#save-rule',
+            cancelRuleSavingButton: '#cancel',
+        },
+        newSectionBtn: '.ant-collapse-header',
+        newSection: {
+            authorityField: '#authority',
+            submitAuthorityBtn: '#authority-submit',
+            attributeNameField: "#name",
+            ruleField: '[data-test-id=rule-form-item]',
+            ruleOptions: {
+              hierarchical: '',
+              permissive:'',
+              restrictive: '',
+            },
+            orderField1: '#order_0',
+            plusOrderButton: '#plus-order-button',
+            minusOrderButton: '#minus-order-button',
+            submitAttributeBtn: '#create-attribute-button',
+        },
+        attributeItem: '.ant-list-item',
+    },
+    entitlementsPage: {
+        authorityNamespaceField:'#authority',
+        attributeNameField: '#name',
+        attributeValueField: '#value',
+        submitAttributeButton: "#assign-submit",
+        entityDetailsPage: {
+            tableCell: '.ant-table-cell',
+            tableRow: '.ant-table-row',
+            deleteEntitlementBtn: '.ant-btn-link >> nth=0',
+            deleteEntitlementModalBtn: '#delete-attr',
+        }
+    },
+    authoritiesPage: {
+        header: '.ant-page-header-heading-title',
+        authoritiesTableRow: '.ant-table-row',
+        deleteAuthorityButton: '#delete-authority-button',
+        confirmDeletionModal: {
+            cancelDeletionBtn: '#cancel-deletion',
+            confirmDeletionBtn: '#delete-authority',
+        }
+    },
+    alertMessage: '.Toastify__toast-body',
+    tokenMessage: '.Toastify__toast'
+}
