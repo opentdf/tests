cfe61935465288ecc62ceeb65a0de6c5bc2d1a8b
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2022-12-29 13:09:40 +0200

    debug

--- abacus-integration-test/e2e/helpers/operations.ts
+++ abacus-integration-test/e2e/helpers/operations.ts
@@ -70,12 +70,13 @@ export const deleteAttributeViaAPI = async (apiContext: APIRequestContext, autho
 };
 
 export const deleteAuthorityViaAPI = async (apiContext: APIRequestContext, authority: string) => {
-  //@ts-ignore
-  const deleteAuthorityResponse = await apiContext.delete('http://localhost:65432/api/attributes/authorities',{
-    data: {
-      "authority": authority
-    },
-  });
-  await expect(deleteAuthorityResponse.status()).toBe(202)
-  await expect(deleteAuthorityResponse.ok()).toBeTruthy()
+  // //@ts-ignore
+  // const deleteAuthorityResponse = await apiContext.delete('http://localhost:65432/api/attributes/authorities',{
+  //   data: {
+  //     "authority": authority
+  //   },
+  // });
+  // await expect(deleteAuthorityResponse.status()).toBe(202)
+  // await expect(deleteAuthorityResponse.ok()).toBeTruthy()
+  return Promise.resolve();
 };
