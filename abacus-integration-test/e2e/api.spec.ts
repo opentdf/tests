7f3329cdf055b88e19d6999ebcc22c52ec47b842
sievdokymov-virtru <sergii.ievdokymov@virtru.com>
2023-01-10 16:28:03 +0200

    Try to access abacus on another port in tests run2

--- abacus-integration-test/e2e/api.spec.ts
+++ abacus-integration-test/e2e/api.spec.ts
@@ -7,7 +7,7 @@ let apiContext: APIRequestContext;
 let pageContext;
 
 const getAccessTokenAfterLogin = async (page: Page) => {
-    await page.goto('http://localhost:80/');
+    await page.goto('http://localhost:8080/');
     await page.locator(selectors.loginButton).click()
     await page.fill(selectors.loginScreen.usernameField, "user1");
     await page.fill(selectors.loginScreen.passwordField, "testuser123");
