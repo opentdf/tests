701c3610afc8892b0d9f19435b8e5fc5242f5d0f
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2024-03-14 12:30:29 +0200

    upd config url

--- xtest/sdk/js/outbound-test-app/playwright.config.ts
+++ xtest/sdk/js/outbound-test-app/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://local.virtru.com/',
+    baseURL: 'http://localhost:65432',
     headless: !!process.env.CI,
     ignoreHTTPSErrors: true,
     acceptDownloads: true,
