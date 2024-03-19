a8c090960ca3610283d87568883499f082ab47bd
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2024-03-19 13:15:25 +0200

    fix state

--- xtest/sdk/js/outbound-test-app/src/index.tsx
+++ xtest/sdk/js/outbound-test-app/src/index.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Keycloak from 'keycloak-js';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
+// import Keycloak from 'keycloak-js';
+// import { ReactKeycloakProvider } from '@react-keycloak/web';
 import './index.css';
 import App from './App';
 
-const keycloak = new Keycloak({
-    realm: 'tdf',
-    url: 'http://localhost:65432/auth/',
-    clientId: 'dcr-test',
-});
+// const keycloak = new Keycloak({
+//     realm: 'tdf',
+//     url: 'http://localhost:65432/auth/',
+//     clientId: 'dcr-test',
+// });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <ReactKeycloakProvider
-      authClient={keycloak}
-      initOptions={{
-        checkLoginIframe: false,
-        responseType: "code id_token token",
-        pkceMethod: "S256"
-      }}
-    >
-      <App />
-    </ReactKeycloakProvider>
+    <React.StrictMode>
+      <div>Outbound Demo React App</div>
+    </React.StrictMode>
 );
+
+// root.render(
+//     <ReactKeycloakProvider
+//         authClient={keycloak}
+//         initOptions={{
+//             checkLoginIframe: false,
+//             responseType: "code id_token token",
+//             pkceMethod: "S256"
+//         }}
+//     >
+//         <App />
+//     </ReactKeycloakProvider>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
