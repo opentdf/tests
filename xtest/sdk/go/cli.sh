269d7e2a00053692f8c1243fe47f027e8487232e
Elizabeth Healy <ehealy@virtru.com>
2024-07-22 15:49:44 -0400

    try with otdfctl

--- xtest/sdk/go/cli.sh
+++ xtest/sdk/go/cli.sh
@@ -10,27 +10,56 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # shellcheck source=../../test.env
 source "$SCRIPT_DIR"/../../test.env
 
-## USES THE PLATFORM/EXAMPLES CLI
-
 args=(
   -o "$3"
-  --creds $CLIENTID:$CLIENTSECRET
-  --platformEndpoint $PLATFORMURL
-  --tokenEndpoint $TOKENENDPOINT
+  --host "$KASURL"
+  --tls-no-verify
+  --log-level debug
+  --with-client-creds '{"clientId":"'$CLIENTID'","clientSecret":"'$CLIENTSECRET'"}'
 )
-
+if [ "$4" == "True" ]; then
+    args+=(--tdf-type nano)
+fi
 if [ "$1" == "encrypt" ]; then
-    if [ "$4" == "True" ]; then
-        args+=(--nano)
-    fi
-    FILE_INPUT=$(cat "$2")
-    echo "$SCRIPT_DIR"/examples encrypt "${args[@]}" --autoconfigure=false "$FILE_INPUT"
-    "$SCRIPT_DIR"/examples encrypt "${args[@]}" --autoconfigure=false "$FILE_INPUT"
+   echo "$SCRIPT_DIR"/otdfctl encrypt "${args[@]}" "$2"  
+   "$SCRIPT_DIR"/otdfctl encrypt "${args[@]}" "$2" 
 elif [ "$1" == "decrypt" ]; then
-    echo "$SCRIPT_DIR"/examples decrypt "${args[@]}" "$2"
-    "$SCRIPT_DIR"/examples decrypt "${args[@]}" "$2"
+    "$SCRIPT_DIR"/otdfctl decrypt "${args[@]}" "$2"
 else
     echo "Incorrect argument provided"
     exit 1
 fi
 
+
+# # Common shell wrapper used to interface to SDK implementation.
+# #
+# # Usage: ./cli.sh <encrypt | decrypt> <src-file> <dst-file> <nano>
+# #
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# # shellcheck source=../../test.env
+# source "$SCRIPT_DIR"/../../test.env
+
+# ## USES THE PLATFORM/EXAMPLES CLI
+
+# args=(
+#   -o "$3"
+#   --creds $CLIENTID:$CLIENTSECRET
+#   --platformEndpoint $PLATFORMURL
+#   --tokenEndpoint $TOKENENDPOINT
+# )
+
+# if [ "$1" == "encrypt" ]; then
+#     if [ "$4" == "True" ]; then
+#         args+=(--nano)
+#     fi
+#     FILE_INPUT=$(cat "$2")
+#     echo "$SCRIPT_DIR"/examples encrypt "${args[@]}" --autoconfigure=false "$FILE_INPUT"
+#     "$SCRIPT_DIR"/examples encrypt "${args[@]}" --autoconfigure=false "$FILE_INPUT"
+# elif [ "$1" == "decrypt" ]; then
+#     echo "$SCRIPT_DIR"/examples decrypt "${args[@]}" "$2"
+#     "$SCRIPT_DIR"/examples decrypt "${args[@]}" "$2"
+# else
+#     echo "Incorrect argument provided"
+#     exit 1
+# fi
