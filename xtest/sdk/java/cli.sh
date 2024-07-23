28442ab9a272ee0955ee2c2d30b65990b64c6fee
Elizabeth Healy <35498075+elizabethhealy@users.noreply.github.com>
2024-07-22 20:07:14 -0400

    Add java to xtest (#154)
    * add java sdk
    
    * linting, update paths
    
    * kas_url after command
    
    * try full kasurl
    
    * test with platform before policy binding update
    
    * back to main

---
+++ xtest/sdk/java/cli.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2206,SC1091
+
+# Common shell wrapper used to interface to SDK implementation.
+#
+# Usage: ./cli.sh <encrypt | decrypt> <src-file> <dst-file> <nano>
+#
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# shellcheck source=../../test.env
+source "$SCRIPT_DIR"/../../test.env
+
+args=(
+  "--client-id=$CLIENTID"
+  "--client-secret=$CLIENTSECRET"
+  "--platform-endpoint=$PLATFORMENDPOINT"
+  -i
+)
+COMMAND="$1"
+if [ "$4" == "nano" ]; then
+    COMMAND="$1"nano
+fi
+args+=("$COMMAND")
+
+if [ "$1" == "encrypt" ]; then
+    args+=(--kas-url=$KASURL)
+fi
+
+echo java -jar "$SCRIPT_DIR"/cmdline.jar "${args[@]}" -f "$2" ">" "$3"
+java -jar "$SCRIPT_DIR"/cmdline.jar "${args[@]}" -f "$2" > "$3"
