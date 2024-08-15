c88cbac0e3c13ae08ab4b8f3a086cc5f19a87552
Dave Mihalcik <dmihalcik@virtru.com>
2024-08-15 16:24:10 -0400

    chore(xtest): Adds ztdf manifest assertions (#162)
    - Validates that manifests are well formed using pydantic written from the spec
    - In abac tests, validates that the KAOs have the format expected for shares and splits

--- xtest/tdfs.py
+++ xtest/tdfs.py
@@ -1,5 +1,8 @@
 import logging
 import subprocess
+import zipfile
+
+from pydantic import BaseModel
 
 logger = logging.getLogger("xtest")
 logging.basicConfig()
@@ -11,6 +14,76 @@ sdk_paths = {
     "java": "sdk/java/cli.sh",
     "js": "sdk/js/cli/cli.sh",
 }
+
+
+class PolicyBinding(BaseModel):
+    alg: str
+    hash: str
+
+
+class KeyAccessObject(BaseModel):
+    type: str
+    url: str
+    protocol: str
+    wrappedKey: str
+    policyBinding: str | PolicyBinding
+    encryptedMetadata: str | None = None
+    kid: str | None = None
+    sid: str | None = None
+    tdf_spec_version: str | None = None
+
+
+class EncryptionMethod(BaseModel):
+    algorithm: str
+    iv: str | None = None
+    isStreamable: bool | None = False
+
+
+class IntegritySignature(BaseModel):
+    alg: str | None = "HS256"
+    sig: str
+
+
+class IntegritySegment(BaseModel):
+    hash: str
+    segmentSize: int | None = None
+    encryptedSegmentSize: int | None = None
+
+
+class Integrity(BaseModel):
+    rootSignature: IntegritySignature
+    segmentHashAlg: str
+    segmentSizeDefault: int | None = 0
+    encryptedSegmentSizeDefault: int | None = 0
+    segments: list[IntegritySegment] | None = None
+
+
+class PayloadReference(BaseModel):
+    type: str
+    url: str
+    protocol: str
+    isEncrypted: bool
+    mimeType: str | None = None
+    tdf_spec_version: str | None = None
+
+
+class EncryptionInformation(BaseModel):
+    type: str
+    policy: str
+    keyAccess: list[KeyAccessObject]
+    method: EncryptionMethod
+    integrityInformation: Integrity
+
+
+class Manifest(BaseModel):
+    encryptionInformation: EncryptionInformation
+    payload: PayloadReference
+
+
+def manifest(tdf_file: str) -> Manifest:
+    with zipfile.ZipFile(tdf_file, "r") as tdfz:
+        with tdfz.open("0.manifest.json") as manifestEntry:
+            return Manifest.model_validate_json(manifestEntry.read())
 
 
 def encrypt(
