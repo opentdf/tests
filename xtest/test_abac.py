8e03ba8f30682f49b1322eee943016e6edccf85c
Dave Mihalcik <dmihalcik@virtru.com>
2024-08-27 13:38:26 -0400

    chore(xtest): Adds nanotdf header validation (#170)
    - Adds canonical parsing of nanoTDF headers based on the current spec
    - Validates thats the nanos parse during nano container round trips
    - Validates that the nanos have a key identifier

--- xtest/test_abac.py
+++ xtest/test_abac.py
@@ -9,39 +9,6 @@ import tdfs
 otdfctl = abac.OpentdfCommandLineTool()
 
 
-def test_namespaces_list():
-    ns = otdfctl.namespace_list()
-    assert len(ns) >= 4
-
-
-def test_attribute_create():
-    random_ns = "".join(random.choices(string.ascii_lowercase, k=8)) + ".com"
-    ns = otdfctl.namespace_create(random_ns)
-    anyof = otdfctl.attribute_create(
-        ns, "free", abac.AttributeRule.ANY_OF, ["1", "2", "3"]
-    )
-    allof = otdfctl.attribute_create(
-        ns, "strict", abac.AttributeRule.ALL_OF, ["1", "2", "3"]
-    )
-    assert anyof != allof
-
-
-def test_scs_create():
-    c = abac.Condition(
-        subject_external_selector_value=".clientId",
-        operator=abac.SubjectMappingOperatorEnum.IN,
-        subject_external_values=["opentdf-sdk"],
-    )
-    cg = abac.ConditionGroup(
-        boolean_operator=abac.ConditionBooleanTypeEnum.OR, conditions=[c]
-    )
-
-    sc = otdfctl.scs_create(
-        [abac.SubjectSet(condition_groups=[cg])],
-    )
-    assert len(sc.subject_sets) == 1
-
-
 def load_cached_kas_keys() -> abac.PublicKey:
     keyset: list[abac.KasPublicKey] = []
     with open("../../platform/kas-cert.pem", "r") as rsaFile:
