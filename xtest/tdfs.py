00315871c1704c112938b0ec33fcf7822201960f
David Mihalcik <dmihalcik@virtru.com>
2024-10-24 17:23:47 -0400

    chore(xtest): Assertion xtest v1

--- xtest/tdfs.py
+++ xtest/tdfs.py
@@ -1,3 +1,4 @@
+import assertions as tdfassertions
 import base64
 from collections.abc import Callable
 import logging
@@ -15,7 +16,7 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 sdk_type = Literal["go", "java", "js"]
 
-feature_type = Literal["autoconfigure", "nano_ecdsa", "ns_grants"]
+feature_type = Literal["assertions", "autoconfigure", "nano_ecdsa", "ns_grants"]
 
 sdk_paths: dict[sdk_type, str] = {
     "go": "sdk/go/cli.sh",
@@ -115,6 +116,7 @@ class EncryptionInformation(BaseModel):
 class Manifest(BaseModel):
     encryptionInformation: EncryptionInformation
     payload: PayloadReference
+    assertions: list[tdfassertions.Assertion] | None = None
 
 
 def manifest(tdf_file: str) -> Manifest:
@@ -153,7 +155,8 @@ def encrypt(
     ct_file,
     mime_type="application/octet-stream",
     fmt="nano",
-    attr_values=[],
+    attr_values: list[str] | None = None,
+    assert_value: list[tdfassertions.Assertion] | None = None,
     use_ecdsa_binding=False,
 ):
     c = [
@@ -191,23 +194,13 @@ def decrypt(sdk, ct_file, rt_file, fmt="nano"):
 
 
 def supports(sdk: sdk_type, feature: feature_type) -> bool:
-    do_check = False
-    if feature == "autoconfigure":
-        if sdk in ["go", "java"]:
+    match (feature, sdk):
+        case ("autoconfigure", ("go" | "java")):
             return True
-        do_check = sdk == "js"
-    elif feature == "nano_ecdsa":
-        if sdk in ["go"]:
+        case ("nano_ecdsa", "go"):
             return True
-        do_check = True
-    elif feature == "ns_grants":
-        if sdk in ["go", "java"]:
+        case ("ns_grants", ("go" | "java")):
             return True
-        do_check = sdk == "js"
-    else:
-        raise ValueError(f"unknown feature {feature}")
-    if not do_check:
-        return False
 
     c = [
         sdk_paths[sdk],
--- xtest/test_tdfs.py
+++ xtest/test_tdfs.py
@@ -1,3 +1,4 @@
+import assertions
 import filecmp
 import os
 import subprocess
@@ -13,15 +14,23 @@ counter = 0
 
 
 def doEncryptWith(
-    pt_file: str, encrypt_sdk: str, container: str, tmp_dir: str, use_ecdsa: bool
+    pt_file: str,
+    encrypt_sdk: str,
+    container: str,
+    tmp_dir: str,
+    use_ecdsa: bool = False,
+    az: list[assertions.Assertion] = [],
+    scenario: str = "",
 ) -> str:
     global counter
     counter = (counter or 0) + 1
     c = counter
     container_id = f"{encrypt_sdk}-{container}"
+    if scenario != "":
+        container_id += f"-{scenario}"
     if container_id in cipherTexts:
         return cipherTexts[container_id]
-    ct_file = f"{tmp_dir}test-{encrypt_sdk}-{c}.{container}"
+    ct_file = f"{tmp_dir}test-{encrypt_sdk}-{scenario}{c}.{container}"
     tdfs.encrypt(
         encrypt_sdk,
         pt_file,
@@ -29,6 +38,7 @@ def doEncryptWith(
         mime_type="text/plain",
         fmt=container,
         use_ecdsa_binding=use_ecdsa,
+        assert_value=az,
     )
     if container == "ztdf":
         manifest = tdfs.manifest(ct_file)
@@ -73,7 +83,7 @@ def breakBinding(manifest: tdfs.Manifest) -> tdfs.Manifest:
 
 
 def test_tdf_with_unbound_policy(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
-    ct_file = doEncryptWith(pt_file, encrypt_sdk, "ztdf", tmp_dir, False)
+    ct_file = doEncryptWith(pt_file, encrypt_sdk, "ztdf", tmp_dir)
     assert os.path.isfile(ct_file)
     b_file = tdfs.update_manifest("unbound_policy", ct_file, breakBinding)
     fname = os.path.basename(b_file).split(".")[0]
@@ -83,3 +93,35 @@ def test_tdf_with_unbound_policy(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
         assert False, "decrypt succeeded unexpectedly"
     except subprocess.CalledProcessError as exc:
         assert b"wrap" in exc.output
+
+
+def test_tdf_assertions(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
+    if not tdfs.supports(encrypt_sdk, "assertions") or not tdfs.supports(
+        decrypt_sdk, "assertions"
+    ):
+        pytest.skip(f"sdk doesn't yet support assertions")
+    ct_file = doEncryptWith(
+        pt_file,
+        encrypt_sdk,
+        "ztdf",
+        tmp_dir,
+        scenario="assertions",
+        az=[
+            assertions.Assertion(
+                id="assertion-1",
+                type="handling",
+                scope="payload",
+                appliesToState="encrypted",
+                statement=assertions.Statement(
+                    format="json",
+                    schema="https://schema.org/Person",
+                    value='{"name":"Alice"}',
+                ),
+            ),
+        ],
+    )
+    assert os.path.isfile(ct_file)
+    fname = os.path.basename(ct_file).split(".")[0]
+    rt_file = f"{tmp_dir}test-{fname}.untdf"
+    tdfs.decrypt(decrypt_sdk, ct_file, rt_file, "ztdf")
+    assert filecmp.cmp(pt_file, rt_file)
