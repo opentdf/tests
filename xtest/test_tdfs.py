b71b16166c991dd1f7711f49c4136ce4f3ae986f
Elizabeth Healy <35498075+elizabethhealy@users.noreply.github.com>
2024-10-28 14:19:10 -0400

    feat: Tamper with root and segment signatures (#216)
    * break root signature
    
    * break segment sig
    
    * verbose
    
    * add separate pr workflow to call xtest wf
    
    * use $
    
    * remove tests ref, breaks pr workflow
    
    * update errors

--- xtest/test_tdfs.py
+++ xtest/test_tdfs.py
@@ -2,6 +2,9 @@ import assertions
 import filecmp
 import os
 import subprocess
+import base64
+import string
+import random
 
 import pytest
 
@@ -82,6 +85,32 @@ def breakBinding(manifest: tdfs.Manifest) -> tdfs.Manifest:
     return manifest
 
 
+def changeLastThree(byt: bytes) -> bytes:
+    new_three = "".join(random.choices(string.ascii_lowercase + string.digits, k=3))
+    if new_three == byt[-3:]:
+        # catch the case where the random string is the same (v unlikely)
+        return changeLastThree(byt)
+    return byt[:-3] + new_three.encode()
+
+
+def breakRootSignature(manifest: tdfs.Manifest) -> tdfs.Manifest:
+    rootSig = manifest.encryptionInformation.integrityInformation.rootSignature.sig
+    alteredSig = base64.b64encode(changeLastThree(base64.b64decode(rootSig)))
+    manifest.encryptionInformation.integrityInformation.rootSignature.sig = alteredSig
+    return manifest
+
+
+def breakSegmentSignature(manifest: tdfs.Manifest) -> tdfs.Manifest:
+    segments = manifest.encryptionInformation.integrityInformation.segments
+    # choose a random segment
+    index = random.randrange(len(segments))
+    segment = segments[index]
+    alteredHash = base64.b64encode(changeLastThree(base64.b64decode(segment.hash)))
+    segment.hash = alteredHash
+    manifest.encryptionInformation.integrityInformation.segments[index] = segment
+    return manifest
+
+
 def test_tdf_with_unbound_policy(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
     ct_file = doEncryptWith(pt_file, encrypt_sdk, "ztdf", tmp_dir)
     assert os.path.isfile(ct_file)
@@ -93,6 +122,32 @@ def test_tdf_with_unbound_policy(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
         assert False, "decrypt succeeded unexpectedly"
     except subprocess.CalledProcessError as exc:
         assert b"wrap" in exc.output
+
+
+def test_tdf_with_altered_root_sig(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
+    ct_file = doEncryptWith(pt_file, encrypt_sdk, "ztdf", tmp_dir)
+    assert os.path.isfile(ct_file)
+    b_file = tdfs.update_manifest("broken_root_sig", ct_file, breakRootSignature)
+    fname = os.path.basename(b_file).split(".")[0]
+    rt_file = f"{tmp_dir}test-{fname}.untdf"
+    try:
+        tdfs.decrypt(decrypt_sdk, b_file, rt_file, "ztdf")
+        assert False, "decrypt succeeded unexpectedly"
+    except subprocess.CalledProcessError as exc:
+        assert b"root" in exc.output
+
+
+def test_tdf_with_altered_seg_sig(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
+    ct_file = doEncryptWith(pt_file, encrypt_sdk, "ztdf", tmp_dir)
+    assert os.path.isfile(ct_file)
+    b_file = tdfs.update_manifest("broken_seg_sig", ct_file, breakSegmentSignature)
+    fname = os.path.basename(b_file).split(".")[0]
+    rt_file = f"{tmp_dir}test-{fname}.untdf"
+    try:
+        tdfs.decrypt(decrypt_sdk, b_file, rt_file, "ztdf")
+        assert False, "decrypt succeeded unexpectedly"
+    except subprocess.CalledProcessError as exc:
+        assert b"signature" in exc.output
 
 
 def test_tdf_assertions(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
