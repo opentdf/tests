1036728d182df4c5ca2a6f5be5052d2ae06735c9
Elizabeth Healy <ehealy@virtru.com>
2024-11-01 10:31:31 -0400

    check for rewrap

--- xtest/test_tdfs.py
+++ xtest/test_tdfs.py
@@ -122,7 +122,7 @@ def test_tdf_with_unbound_policy(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
         assert False, "decrypt succeeded unexpectedly"
     except subprocess.CalledProcessError as exc:
         assert b"wrap" in exc.output
-        # assert b"tamper" in exc.output
+        assert (b"tamper" in exc.output or b"InvalidFileError" in exc.output)
 
 
 def test_tdf_with_altered_root_sig(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
