23b241df2d073ebc3ec33706a18fbc83a862c66a
Elizabeth Healy <ehealy@virtru.com>
2024-11-08 14:31:39 -0500

    remove specific wording

--- xtest/test_tdfs.py
+++ xtest/test_tdfs.py
@@ -121,8 +121,11 @@ def test_tdf_with_unbound_policy(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
         tdfs.decrypt(decrypt_sdk, b_file, rt_file, "ztdf")
         assert False, "decrypt succeeded unexpectedly"
     except subprocess.CalledProcessError as exc:
-        assert b"wrap" in exc.output
-        assert b"tamper" in exc.output or b"InvalidFileError" in exc.output or "invalid TDF" in exc.output
+        assert (
+            b"tamper" in exc.output
+            or b"InvalidFileError" in exc.output
+            or "invalid TDF" in exc.output
+        )
 
 
 def test_tdf_with_altered_root_sig(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
@@ -135,8 +138,11 @@ def test_tdf_with_altered_root_sig(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
         tdfs.decrypt(decrypt_sdk, b_file, rt_file, "ztdf")
         assert False, "decrypt succeeded unexpectedly"
     except subprocess.CalledProcessError as exc:
-        assert b"root" in exc.output
-        assert b"tamper" in exc.output or b"IntegrityError" in exc.output or "invalid TDF" in exc.output
+        assert (
+            b"tamper" in exc.output
+            or b"IntegrityError" in exc.output
+            or "invalid TDF" in exc.output
+        )
 
 
 def test_tdf_with_altered_seg_sig(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
@@ -149,8 +155,11 @@ def test_tdf_with_altered_seg_sig(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
         tdfs.decrypt(decrypt_sdk, b_file, rt_file, "ztdf")
         assert False, "decrypt succeeded unexpectedly"
     except subprocess.CalledProcessError as exc:
-        assert b"signature" in exc.output
-        assert b"tamper" in exc.output or b"IntegrityError" in exc.output or "invalid TDF" in exc.output
+        assert (
+            b"tamper" in exc.output
+            or b"IntegrityError" in exc.output
+            or "invalid TDF" in exc.output
+        )
 
 
 def test_tdf_assertions(encrypt_sdk, decrypt_sdk, pt_file, tmp_dir):
