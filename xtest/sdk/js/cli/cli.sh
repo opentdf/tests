87e0fdf4efadbdcb4936067b0f5c8dc31484a8b3
Morgan Kleene <mkleene@virtru.com>
2024-12-20 16:37:56 -0500

    no need for them first

--- xtest/sdk/js/cli/cli.sh
+++ xtest/sdk/js/cli/cli.sh
@@ -70,7 +70,7 @@ if [ "$1" == "encrypt" ]; then
 
   npx $CTL encrypt "$2" "${args[@]}"
 elif [ "$1" == "decrypt" ]; then
-  printf 'performing decryption with [%s] [%s] [%s]\n' $CTL "$2" "${args[@]}"
+  printf 'decrypt [%s]\n' "${args[@]}"
   npx $CTL decrypt "$2" "${args[@]}"
 else
   echo "Incorrect argument provided"
