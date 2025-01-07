8a045eff3b344981ee8e6d998259d6d5fa4ae23c
Morgan Kleene <mkleene@virtru.com>
2025-01-07 16:51:11 -0500

    chore(sdk): add legacy decryption tests (#233)
    specify a fixed key at when starting the platform and use it to decrypt files created by an old SDK
    
    requires https://github.com/opentdf/platform/pull/1830 in order to specify custom KAS keys
    
    the file name is `<descriptive>-<sdk>-<tdf version>-<git hash of sdk commit>.tdf`

---
+++ xtest/test_legacy.py
@@ -0,0 +1,41 @@
+import os
+
+import tdfs
+
+
+def get_golden_file(golden_file_name: str) -> str:
+    xtest_dir = os.path.dirname(os.path.realpath(__file__))
+    filename = os.path.join(xtest_dir, "golden", f"{golden_file_name}")
+    if os.path.isfile(filename):
+        return filename
+    raise FileNotFoundError(f"Golden file '{filename}' not found.")
+
+
+def test_decrypt_small(
+    decrypt_sdk: tdfs.sdk_type,
+    tmp_dir,
+):
+    ct_file = get_golden_file("small-java-4.3.0-e0f8caf.tdf")
+    rt_file = os.path.join(tmp_dir, "small-java.untdf")
+    tdfs.decrypt(decrypt_sdk, ct_file, rt_file, fmt="ztdf")
+    file_stats = os.stat(rt_file)
+    assert file_stats.st_size == 5 * 2**10
+    expected_bytes = bytes([0] * 1024)
+    with open(rt_file, "rb") as f:
+        while b := f.read(1024):
+            assert b == expected_bytes
+
+
+def test_decrypt_big(
+    decrypt_sdk: tdfs.sdk_type,
+    tmp_dir,
+):
+    ct_file = get_golden_file("big-java-4.3.0-e0f8caf.tdf")
+    rt_file = os.path.join(tmp_dir, "big-java.untdf")
+    tdfs.decrypt(decrypt_sdk, ct_file, rt_file, fmt="ztdf")
+    file_stats = os.stat(rt_file)
+    assert file_stats.st_size == 10 * 2**20
+    expected_bytes = bytes([0] * 1024)
+    with open(rt_file, "rb") as f:
+        while b := f.read(1024):
+            assert b == expected_bytes
