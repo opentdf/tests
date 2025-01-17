5fdf86a59f9c80b357de82d1081a5c59a643a083
Morgan Kleene <mkleene@virtru.com>
2025-01-17 17:15:16 -0500

    fix(sdk): add a test case with empty KAOs (#240)
    Adds a file that has a manifest with KAOs with empty/missing split IDs. The key is only available from the second URL:
    ```json
    {
      "encryptionInformation": {
        "type": "split",
        "policy": "eyJ1dWlkIjoiMWJjZWZjNDQtODgzYS00ZjEzLWE3NGMtNTExYmE1ZWY4NjQ3IiwiYm9keSI6eyJkYXRhQXR0cmlidXRlcyI6W10sImRpc3NlbSI6W119fQ==",
        "keyAccess": [
          {
            "type": "wrapped",
            "url": "http://doesnotexist.example.org:8080/kas",
            "protocol": "kas",
            "wrappedKey": "dGhpcyBpcyBzb21lIGRhdGE=",
            "policyBinding": {
              "alg": "HS256",
              "hash": "ZTVlZTBkYWQyYTljMjA1ZWNkNjM1NDc0ODY0MTE1MTE5YjFiZDcwMDk4YTAxM2YzZjNiYzliNzAwNGZiOGU5YQ=="
            },
            "encryptedMetadata": "",
            "kid": "golden-r1"
          },
          {
            "type": "wrapped",
            "url": "http://localhost:8080/kas",
            "protocol": "kas",
            "wrappedKey": "XDKy+uP14krfViNBIfSWTlN5+1wK8+dV+v715fT1EJO5kuUxDaGiqOEJ2Tjnb2B/VQT0IAtCeXjQEm/4KxuMRoYJZeXQD3vs4+ZMxJz8aDnAzOMmIwKiEri8H8LYO/sZ448LJXHCzGQmmNO2Pi++GnRbU67BzbgZZHXOV1HdIFS8I+vaCzbZR13/TIZNKjRrwyE27vLRRWNW3yBaxpeD1X+qHVqFO5Xq6eADmq4NTH1B2iHdAoN11ChHDI9e09dywETdFUdPlToW+lS7tCAcLRxPNBaijtEX+HNULw7p6lHOOa/8Mt3ZLkLUko/rOTKSEQyC8jNN/rl77iaVSeYcaQ==",
            "policyBinding": {
              "alg": "HS256",
              "hash": "ZTVlZTBkYWQyYTljMjA1ZWNkNjM1NDc0ODY0MTE1MTE5YjFiZDcwMDk4YTAxM2YzZjNiYzliNzAwNGZiOGU5YQ=="
            },
            "encryptedMetadata": "",
            "kid": "golden-r1"
          },
          {
            "type": "wrapped",
            "url": "http://alsodoesnotexist.example.org:8080/kas",
            "protocol": "kas",
            "wrappedKey": "dGhpcyBpcyBzb21lIGRhdGE=",
            "policyBinding": {
              "alg": "HS256",
              "hash": "ZTVlZTBkYWQyYTljMjA1ZWNkNjM1NDc0ODY0MTE1MTE5YjFiZDcwMDk4YTAxM2YzZjNiYzliNzAwNGZiOGU5YQ=="
            },
            "encryptedMetadata": "",
            "kid": "golden-r1",
            "sid": ""
          }
        ]
      }
    }
    
    ```

--- xtest/test_legacy.py
+++ xtest/test_legacy.py
@@ -26,6 +26,21 @@ def test_decrypt_small(
             assert b == expected_bytes
 
 
+def test_decrypt_no_splitid(
+    decrypt_sdk: tdfs.sdk_type,
+    tmp_dir,
+):
+    ct_file = get_golden_file("no-splitids-java.tdf")
+    rt_file = os.path.join(tmp_dir, "no-splitids-java.untdf")
+    tdfs.decrypt(decrypt_sdk, ct_file, rt_file, fmt="ztdf")
+    file_stats = os.stat(rt_file)
+    assert file_stats.st_size == 5 * 2**10
+    expected_bytes = bytes([0] * 1024)
+    with open(rt_file, "rb") as f:
+        while b := f.read(1024):
+            assert b == expected_bytes
+
+
 def test_decrypt_big(
     decrypt_sdk: tdfs.sdk_type,
     tmp_dir,
