08121cb72af03e585e8a3742acf1c577e7b5239c
David Mihalcik <dmihalcik@virtru.com>
2025-01-17 11:42:50 -0500

    chore(xtest): Allow hexless mode in go

--- xtest/sdk/go/cli.sh
+++ xtest/sdk/go/cli.sh
@@ -28,6 +28,10 @@ if [ "$1" == "supports" ]; then
       "${cmd[@]}" help decrypt | grep with-assertion-verification-keys
       exit $?
       ;;
+    hexless)
+      "${cmd[@]}" --version --json | jq -r .schema_version | awk -F. '{ if ($1 > 4 || ($1 == 4 && $2 > 2) || ($1 == 4 && $2 == 3 && $3 >= 0)) exit 0; else exit 1; }'
+      exit $?
+      ;;
     *)
       echo "Unknown feature: $2"
       exit 2
