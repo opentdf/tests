b94a4c1687820a30c6123d1b297a4f51ca154baa
Morgan Kleene <mkleene@virtru.com>
2025-01-24 11:12:48 -0500

    add a test that has assertion statement values as objects

--- xtest/test_legacy.py
+++ xtest/test_legacy.py
@@ -26,6 +26,21 @@ def test_decrypt_small(
             assert b == expected_bytes
 
 
+def test_decrypt_big(
+    decrypt_sdk: tdfs.sdk_type,
+    tmp_dir,
+):
+    ct_file = get_golden_file("big-java-4.3.0-e0f8caf.tdf")
+    rt_file = os.path.join(tmp_dir, "big-java.untdf")
+    tdfs.decrypt(decrypt_sdk, ct_file, rt_file, fmt="ztdf")
+    file_stats = os.stat(rt_file)
+    assert file_stats.st_size == 10 * 2**20
+    expected_bytes = bytes([0] * 1024)
+    with open(rt_file, "rb") as f:
+        while b := f.read(1024):
+            assert b == expected_bytes
+
+
 def test_decrypt_no_splitid(
     decrypt_sdk: tdfs.sdk_type,
     tmp_dir,
@@ -41,16 +56,13 @@ def test_decrypt_no_splitid(
             assert b == expected_bytes
 
 
-def test_decrypt_big(
+def test_decrypt_object_json(
     decrypt_sdk: tdfs.sdk_type,
     tmp_dir,
 ):
-    ct_file = get_golden_file("big-java-4.3.0-e0f8caf.tdf")
-    rt_file = os.path.join(tmp_dir, "big-java.untdf")
+    ct_file = get_golden_file("with-json-object-assertions-java.tdf")
+    rt_file = os.path.join(tmp_dir, "with-json-object-assertions-java.untdf")
     tdfs.decrypt(decrypt_sdk, ct_file, rt_file, fmt="ztdf")
     file_stats = os.stat(rt_file)
-    assert file_stats.st_size == 10 * 2**20
-    expected_bytes = bytes([0] * 1024)
     with open(rt_file, "rb") as f:
-        while b := f.read(1024):
-            assert b == expected_bytes
+        assert f.read() == "text"
