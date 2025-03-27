c0120de48adcc9e239e347de44f2ed00c7b2959a
David Mihalcik <dmihalcik@virtru.com>
2025-03-27 15:42:53 -0400

    Update checkout-all.sh

--- xtest/sdk/scripts/checkout-all.sh
+++ xtest/sdk/scripts/checkout-all.sh
@@ -5,7 +5,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 for sdk in go java js; do
-  if ! $SCRIPT_DIR/checkout-sdk-branch.sh $sdk main; then
+  if ! "$SCRIPT_DIR/checkout-sdk-branch.sh" "$sdk" main; then
     echo "Failed to checkout $sdk main branch"
     exit 1
   fi
