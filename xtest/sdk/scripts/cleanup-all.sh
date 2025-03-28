059257eed0cf3372e567bdd4fcfbd13792cc1699
David Mihalcik <dmihalcik@virtru.com>
2025-03-28 14:34:12 -0400

    Update cleanup-all.sh

--- xtest/sdk/scripts/cleanup-all.sh
+++ xtest/sdk/scripts/cleanup-all.sh
@@ -7,7 +7,7 @@ for sdk in go java js; do
   rm -rf "$SCRIPT_DIR/../$sdk/dist"
   for branch in "$SCRIPT_DIR/../${sdk}/src/"*; do
     # Check if the path ends with .git
-    if [[ "$branch" == *.git ]]; then
+    if [[ $branch == *.git ]]; then
       continue
     fi
     if [ -d "$branch" ]; then
