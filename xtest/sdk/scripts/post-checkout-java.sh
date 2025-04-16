54095749e6d4be450efc12e4fe1a3a6996e1ed49
David Mihalcik <dmihalcik@virtru.com>
2025-04-16 19:52:23 -0400

    Update post-checkout-java.sh

--- xtest/sdk/scripts/post-checkout-java.sh
+++ xtest/sdk/scripts/post-checkout-java.sh
@@ -8,6 +8,13 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 BASE_DIR="$SCRIPT_DIR/../java/src"
 
+# Detect the operating system to use the correct sed syntax
+if [[ "$(uname)" == "Darwin" ]]; then
+  SED_CMD="sed -i ''"
+else
+  SED_CMD="sed -i"
+fi
+
 # Loop through all subdirectories in the base directory
 find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type d -not -name "*.git" | while read -r SRC_DIR; do
   POM_FILE="$SRC_DIR/sdk/pom.xml"
@@ -27,12 +34,12 @@ find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type d -not -name "*.git" | while read
   echo "Updating $POM_FILE..."
 
   # Add the platform.branch property to the <properties> section
-  sed -i '' '/<properties>/a \
+  $SED_CMD '/<properties>/a \
         <platform.branch>main</platform.branch>' "$POM_FILE"
 
   # Replace hardcoded branch=main with branch=${platform.branch} in the maven-antrun-plugin configuration
   # shellcheck disable=SC2016 # Literal $; it is for a variable expansion in the maven file
-  sed -i '' 's/branch=main/branch=${platform.branch}/g' "$POM_FILE"
+  $SED_CMD 's/branch=main/branch=${platform.branch}/g' "$POM_FILE"
 done
 
 echo "Update complete."
