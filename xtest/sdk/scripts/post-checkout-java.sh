20373d6d3d2fa2fe6e1ebca2d10b44cdf428f497
David Mihalcik <dmihalcik@virtru.com>
2025-04-16 17:02:44 -0400

    chore(xtest): Let main java-sdk target plat main
    This uses the platform.branch build property in the java-sdk build system to build appropriate protocol buffers as needed.
    
    1. For skew tests with older builds, where the property wasn't defined, retro-actively add it and pin to an appropriate tag. (Right now, this is the tag that was last added when the java tag was released, but I suspect we will need something more nuanced)
    2. For tests from java-sdk pull requests, use the main version of plat when testing against platform head; for tests with released platforms, use the baked version
    3. For tests from platform pull requests, let the head version of the java sdk use the platform head; for tests with released java-sdk, use the baked in version
    
    This invoves:
    
    - extending resolve-version.py with a lookup table matchching old releases of java-sdk to the appropriate release of protocol/go
    - adding an optional 'post-checkout' pass that performs modifications to the source code (in this case, adding the missing maven property)
    - updating the build phase to enable taking in [version].env lists of variables to configure custom make options per-checked out source code

---
+++ xtest/sdk/scripts/post-checkout-java.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Post checkout cleanups for java
+# Currently, this inserts the missing `platform.branch` property into the pom.xml files
+# on older branches that do not have it defined.
+
+# Base directory for the script
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+BASE_DIR="$SCRIPT_DIR/../java/src"
+
+# Loop through all subdirectories in the base directory
+find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type d -not -name "*.git" | while read -r SRC_DIR; do
+  POM_FILE="$SRC_DIR/sdk/pom.xml"
+
+  # Skip if path or file does not exist
+  if [[ ! -f $POM_FILE ]]; then
+    echo "No pom.xml file found in $SRC_DIR, skipping."
+    continue
+  fi
+
+  # Check if the platform.branch property is already defined
+  if grep -q "<platform.branch>" "$POM_FILE"; then
+    echo "platform.branch already defined in $POM_FILE, skipping."
+    continue
+  fi
+
+  echo "Updating $POM_FILE..."
+
+  # Add the platform.branch property to the <properties> section
+  sed -i '' '/<properties>/a \
+        <platform.branch>main</platform.branch>' "$POM_FILE"
+
+  # Replace hardcoded branch=main with branch=${platform.branch} in the maven-antrun-plugin configuration
+  # shellcheck disable=SC2016 # Literal $; it is for a variable expansion in the maven file
+  sed -i '' 's/branch=main/branch=${platform.branch}/g' "$POM_FILE"
+done
+
+echo "Update complete."
