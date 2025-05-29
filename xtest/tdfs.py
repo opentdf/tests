f5ea234a3d937774489de759c2a32def04432688
David Mihalcik <dmihalcik@virtru.com>
2025-05-29 10:44:33 -0400

    Revert "DO NOT SUBMIT sample pyright fail"
    This reverts commit a517998a3754c2da55a2785cfdff1726f0d2da5c.

--- xtest/tdfs.py
+++ xtest/tdfs.py
@@ -90,7 +90,6 @@ class PlatformFeatureSet(BaseModel):
         if self.semver >= (0, 4, 39):
             self.features.add("hexless")
             self.features.add("hexaflexible")
-            self.features.add("superhexaflexible")
 
         if self.semver >= (0, 4, 23):
             self.features.add("nano_ecdsa")
