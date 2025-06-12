89de127afc9f1c3e745a4241e941e20d1631b9a9
sievdokymov-virtru <sergii.ievdokymov@virtru.com>
2025-06-12 16:52:13 +0300

    Try decryption test for 0.7.5 file2

--- xtest/test_legacy.py
+++ xtest/test_legacy.py
@@ -33,7 +33,7 @@ def test_decrypt_small(
             assert b == expected_bytes
 
 def test_decrypt_SDKv0_7_5(
-    decrypt_sdk: tdfs.SDK,)
+    decrypt_sdk: tdfs.SDK,
     tmp_dir: Path,
     in_focus: set[tdfs.SDK],
 ):
