cb3576ea8bfc5bdb9d431f104793384a3f5d8e0a
Dave Mihalcik <dmihalcik@virtru.com>
2025-07-08 10:18:56 -0400

    chore(xtest): Prefer pathless kas uris (#320)
    While I'm here, also let the 'self tests' run on all PRs

--- xtest/test_tdfs.py
+++ xtest/test_tdfs.py
@@ -495,7 +495,7 @@ def change_payload_end(payload_bytes: bytes) -> bytes:
 def malicious_kao(manifest: tdfs.Manifest) -> tdfs.Manifest:
     assert manifest.encryptionInformation.keyAccess
     manifest.encryptionInformation.keyAccess[0].url = (
-        "http://localhost:8585/malicious/kas"  # nothing running at 8585
+        "http://localhost:8585/malicious"  # nothing running at 8585
     )
     return manifest
 
