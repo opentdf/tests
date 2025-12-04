8097ad84e5b5d65b2783b907b054063ffce303c7
sievdokymov-virtru <sergii.ievdokymov@virtru.com>
2025-12-04 14:34:49 +0200

    File name update

--- xtest/test_legacy.py
+++ xtest/test_legacy.py
@@ -97,7 +97,7 @@ def test_decrypt_SDKv0_9_0(
         pytest.skip("Not in focus")
     if not decrypt_sdk.supports("hexless"):
         pytest.skip("Decrypting hexless files is not supported")
-    ct_file = get_golden_file("xstext-java-v0.9.0-2de6a49-DSP2.0.5.1.tdf")
+    ct_file = get_golden_file("xstext-java-v0.9.0-2de6a49-DSP2.0.5.1_and_2.0.6.tdf")
     rt_file = tmp_dir / "0.9.0-java.untdf"
     decrypt_sdk.decrypt(ct_file, rt_file, container="ztdf")
     file_stats = os.stat(rt_file)
