afd3cb6fd5b4f94916d02995380c7c9787e64b3f
Dave Mihalcik <dmihalcik@virtru.com>
2026-06-22 14:27:00 -0400

    fix(xtest): Add secp256r1-mlkem768 mechanism to java cli

--- xtest/sdk/java/cli.sh
+++ xtest/sdk/java/cli.sh
@@ -102,6 +102,11 @@ if [ "$1" == "supports" ]; then
       java -jar "$SCRIPT_DIR"/cmdline.jar --version | jq -re .version | awk -F. '{ if ($1 > 0 || ($1 == 0 && $2 >= 13)) exit 0; else exit 1; }'
       exit $?
       ;;
+    mechanism-mechanism-secpmlkem)
+      set -o pipefail
+      java -jar "$SCRIPT_DIR"/cmdline.jar help encrypt | grep -i secp256r1-mlkem768
+      exit $?
+      ;;
     mechanism-xwing)
       set -o pipefail
       java -jar "$SCRIPT_DIR"/cmdline.jar help encrypt | grep -i xwing
